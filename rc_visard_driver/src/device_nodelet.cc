8c1554a2c0f2696bae0b11c60c93411a3550c4d5
Heiko Hirschmueller <heiko.hirschmueller@roboception.de>
2017-10-02 22:07:44 +0200

    Check and correct quality dynamic reconfigure parameter

--- rc_visard_driver/src/device_nodelet.cc
+++ rc_visard_driver/src/device_nodelet.cc
@@ -351,6 +351,13 @@ void DeviceNodelet::reconfigure(rc_visard_driver::rc_visard_driverConfig &c, uin
 {
   mtx.lock();
 
+  c.quality=c.quality.substr(0, 1);
+
+  if (c.quality[0] != 'L' && c.quality[0] != 'M' && c.quality[0] != 'H' && c.quality[0] != 'S')
+  {
+    c.quality="H";
+  }
+
   config=c;
   level|=l;
 
